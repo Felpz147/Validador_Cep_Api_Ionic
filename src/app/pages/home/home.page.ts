ddfd0c438c1d28228a57890a1ba03bdbcc20743e
Felipe de Queiroz Fresta <felipefresta@gmail.com>
2024-04-05 20:22:12 -0300

    Subindo projeto finalizado do API CEP

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CepService } from '../../services/cep.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  correio: any = {};
+  constructor(private cepService: CepService) {}
+  localizarCep(evento: any){
+
+    let codigoCep: string = evento.detail.value;
+    this.cepService.localizarCep(codigoCep)
+    .then((response: any) =>{
+      this.correio=response;
+      console.log(this.correio);
+    })
+  }
+
+
+
+}
